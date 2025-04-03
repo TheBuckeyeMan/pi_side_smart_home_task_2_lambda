97152ddf95c7eea5de326ca4bdee92cee086a89d
Adam Maas <adamjm1220@gmail.com>
2025-04-03 10:14:33 -0400

    Updated Lambda FUnctionality

--- src/main/java/com/example/app/service/ServiceTrigger.java
+++ src/main/java/com/example/app/service/ServiceTrigger.java
@@ -14,10 +14,12 @@ public class ServiceTrigger {
     private static final Logger log = LoggerFactory.getLogger(ServiceTrigger.class);
     private final GetCertificates getCertificates;
     private final CheckThingExists checkThingExists;
+    private final CheckSerialNumber checkSerialNumber;
 
-    public ServiceTrigger(GetCertificates getCertificates, CheckThingExists checkThingExists){
+    public ServiceTrigger(GetCertificates getCertificates, CheckThingExists checkThingExists, CheckSerialNumber checkSerialNumber){
         this.getCertificates = getCertificates;
         this.checkThingExists = checkThingExists;
+        this.checkSerialNumber = checkSerialNumber;
     }
 
     @Value("${spring.profiles.active}")
@@ -40,6 +42,21 @@ public class ServiceTrigger {
         //Initialization Logs
         log.info("Begining processing of the Lambda for Raspberri Pi Task 2...");
         log.info("The Active Environment is set to: " + environment);
+        try{
+            boolean serialNumberRegistered = checkSerialNumber.isDeviceRegistered(input, dynamoDBTable);
+
+            if (serialNumberRegistered == true){
+                log.info("Serial number is Registered, Issuing certificate!");
+                Map<String, Object> certs = getCertificates.setUpDevice(input);
+                return certs;
+            } else {
+                return Map.of("message", "Device is NOT A REGISTERED SERIAL NUMBER, ensure this device is registered before requesting certs.");
+            }
+
+        } catch (Exception e){
+            log.error("Error occured while attempting to make the certificate", e.getMessage(), e);
+        }
+
 
         //Get the certificates, Set up the THING, Associate the policy with the cert, then the cert with the thing - DOES NOT USE PROVISIONING TEMPLATE
         Map<String, Object> certs = getCertificates.setUpDevice(input);
